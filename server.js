344c87467fd5bcc0089ca1eddf02e54d6c27ee7a
KameronsCodeSpace <kameronmontague@pursuit.org>
2019-10-14 20:58:23 -0400

    I can now randomly get a num from a lower and higher number range

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const bodyParser = require('body-parser');
 const port = 1337;
 const app = express();
 const species = ['lion', 'tiger', 'bear', 'monkey', 'bird', 'whale', 'fox', 'dog', 'deer'];
+const selectedNumbers = [];
 
 app.use(bodyParser.urlencoded({
     extended: false,
@@ -22,10 +23,10 @@ const hello = (req, res, next) => {
 }
 
 const isAnimal = (req, res, next) => {
-    const query = req.params.type;
+    const param = req.params.type;
     console.log(query)
     for(let value of species) {
-        if (query === value) {
+        if (param === value) {
             res.json({
                 status: "success",
                 message: true 
@@ -43,13 +44,29 @@ const isAnimal = (req, res, next) => {
 
 const getRandom = (req, res, next) => {
 
+    let randomNum =  Math.floor(Math.random() * selectedNumbers.length - 1);
+
+    res.json(
+        selectedNumbers[randomNum]
+    )
+}
+
+const generateSpread = (req, res, next) => {
+    const floor = req.params.floor;
+    const ceil = req.params.ceil;
+
+    for(let i = floor; i <= ceil; i++) {
+        selectedNumbers.push(i)
+    }
+    next();
+
 }
 
 app.get('/', hello);
 
 app.get('/animal/:type', isAnimal);
 
-app.get('/random/:floor/:ceil', getRandom);
+app.get('/random/:floor/:ceil',generateSpread, getRandom);
 
 app.listen(port, () => {
     console.log(`http://localhost:${port}`)
